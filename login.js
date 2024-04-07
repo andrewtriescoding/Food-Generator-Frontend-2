727fb5e8a006051067d61fe80cdcd81281f2f8e3
andrewtriescoding <andrewang818w@gmail.com>
2024-04-07 02:43:59 -0700

    hello

---
+++ login.js
@@ -0,0 +1,24 @@
+const submitBtn = document.getElementById('submitBtn')
+
+const userLogin = async () => {
+    try {
+        const response = await fetch(localhost:8000/login, {
+            method: 'POST',
+            body: JSON.stringify({
+                username: this.username,
+                password: this.password
+            }),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            return jsonResponse
+        } 
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+submitBtn.addEventListener('click', userLogin)
